f90b3589b44d15371b6eddfe7a2e3089c4bb9a2b
mguenster <mguenster@studenten.hs-bremerhaven.de>
2017-05-22 13:19:45 +0200

    Aufgabe7_1b

---
+++ Aufgabe7/7_1/b/src/aufgabe7_1b/Gabel.java
@@ -0,0 +1,35 @@
+package aufgabe7_1b;
+
+public class Gabel {
+
+    private int gabelNummer = 0;
+    private boolean gabelInBenutzung = false;
+
+
+    public Gabel(int gabelNummer) {
+        this.gabelNummer = gabelNummer;
+    }
+
+
+    public synchronized void aufnehmen(int nummer, String Seite) throws InterruptedException {
+
+        PruefeGabelInBenutzung();
+        System.out.println("Philosoph: " + nummer + " nimmt auf " + Seite);
+        this.gabelInBenutzung = true;
+        notifyAll();
+
+    }
+
+    public void ablegen(int nummer, String Seite) {
+        System.out.println("Philosoph: " + nummer + " legt ab " + Seite);
+        this.gabelInBenutzung = false;
+    }
+
+    public synchronized void PruefeGabelInBenutzung() throws InterruptedException {
+
+        while (this.gabelInBenutzung){
+            wait();
+        }
+    }
+
+}
